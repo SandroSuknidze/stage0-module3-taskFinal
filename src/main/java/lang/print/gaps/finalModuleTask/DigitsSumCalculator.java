d123d616a49eb46d44ce7d79e259e883d3935a87
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-09 23:30:40 +0400

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int digitOne = number % 10;
+        int digitTwo = (number - digitOne) / 10 % 10;
+        int digitThree = ((number - digitOne) / 10 - digitTwo) / 10 % 10;
+        int digitFour = (((number - digitOne) / 10 - digitTwo) / 10 - digitThree) / 10;
+        System.out.println(digitOne + digitTwo + digitThree + digitFour);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int digitOne = number % 10;
+        int digitTwo = (number - digitOne) / 10 % 10;
+        int digitThree = ((number - digitOne) / 10 - digitTwo) / 10 % 10;
+        System.out.println("" + digitOne + digitTwo + digitThree);
     }
 }
